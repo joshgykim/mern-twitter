86e49abc3eb239e10951c7425d34358b599f5dca
Josh Kim-Biggs <joshgykim@gmail.com>
2021-10-09 17:33:25 -0700

    Begin user-auth

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const mongoose = require('mongoose');
-
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-
-app.get("/", (req, res) => res.send("Hello World changed"));
+  
+app.use(express.json()); 
+app.use(express.urlencoded());
+app.get("/", (req, res) => {
+  debugger;
+  res.send("Hello World changed")
+});
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
